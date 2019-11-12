8209289c02a006c905726fc727ef8c12009674fd
UtkarshDighe <utkarsh@helpshift.com>
2019-11-13 02:23:03 +0530

    Added a new View Controller for showing Answer with custom class to AnswerViewController, thus added required cocoa touch class file.
    Added a segue between two view controller and gave it a identifier name, so that it will be useful while passing values from one view controller to another.

---
+++ SearchSO/AnswerViewController.swift
@@ -0,0 +1,18 @@
+//
+//  AnswerViewController.swift
+//  SearchSO
+//
+//  Created by utkarsh on 13/11/19.
+//  Copyright © 2019 utkarsh. All rights reserved.
+//
+
+import UIKit
+
+class AnswerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+}
