74f878442bd9f8448c2ec373b6436a3d352e1ea7
UtkarshDighe <utkarsh@helpshift.com>
2019-11-13 01:46:43 +0530

    Added identifier name to prototype cell in storyboard.
    Added function searchBarSearchButtonClicked in extension which gets executed when user clicks search button. It calls updateQuestionsToShow method of QuestionsViewController to update questionsToShow property. updateQuestionsToShow calls reload table view to display the result.
    
    Extension to conform UITableViewDelegate and UITableViewDataSource takes care of having necessary function to update table view.

--- SearchSO/QuestionsViewController.swift
+++ SearchSO/QuestionsViewController.swift
@@ -19,10 +19,67 @@ class QuestionsViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         searchBar.delegate = self
+        questionsTableView.dataSource = self
+        questionsTableView.delegate = self
+
+    }
+    
+    func updateQuestionsToShow(_ questions: [Question]) {
+        self.questionsToShow.removeAll()
+        self.questionsToShow = questions
+        DispatchQueue.main.async {
+            self.questionsTableView.reloadData();
+        }
     }
 
 }
 
 extension QuestionsViewController: UISearchBarDelegate {
     
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar)
+    {
+        guard let searchText = searchBar.text else {
+            print("Empty")
+            return
+        }
+        
+        searchBar.resignFirstResponder()
+        
+        let questionsResource = StackOverflowResource<StackOverflowQuestionResponse>(getObject: .questions(searchText), parseJSON: { data in
+            try? JSONDecoder().decode(StackOverflowQuestionResponse.self, from: data)})
+        
+        if let questionsResource = questionsResource {
+            URLSession.shared.load(questionsResource) { questionsInResponse in
+                if let questions = questionsInResponse?.items {
+                    self.updateQuestionsToShow(questions)
+                }
+            }
+        }
+    }
 }
+
+extension QuestionsViewController: UITableViewDelegate {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+}
+
+extension QuestionsViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.questionsToShow.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "LabeledCell", for: indexPath) as! CustomTableViewCell
+        
+        let question = self.questionsToShow[indexPath.row]
+        cell.questionTitle?.text = question.title
+        cell.questionScore?.text = String(question.score)
+        cell.votesText.text = "Votes"
+        
+        return cell
+    }
+}
+
+
