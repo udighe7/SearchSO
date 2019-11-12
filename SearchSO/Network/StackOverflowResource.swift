2ab7e7abd6f2795acd9e586a9aac6f431ebb054b
UtkarshDighe <utkarsh@helpshift.com>
2019-11-13 00:08:55 +0530

    Added a fail-able initialiser in extension which would initialise URL for StackOverflow resource according to StackOverflowObject required by user. And parse closure can be passed according to objects which expected to return from json parsing.

--- SearchSO/Network/StackOverflowResource.swift
+++ SearchSO/Network/StackOverflowResource.swift
@@ -17,3 +17,23 @@ struct StackOverflowResource<A> {
     let url: URL
     let parse: (Data) -> A?
 }
+
+extension StackOverflowResource where A: Decodable {
+    init?(getObject: StackOverflowObject, parseJSON: @escaping (Data) -> A?) {
+        
+        switch getObject {
+        case .questions(let searchText):
+            guard let url = URL(string:"https://api.stackexchange.com/2.2/search/advanced?order=desc&sort=relevance&q="+searchText.replacingOccurrences(of: " ", with: "+")+"%20&site=stackoverflow") else {
+                return nil
+            }
+            self.url = url
+        case .answer(let questionId):
+            guard let url = URL(string: "https://api.stackexchange.com/2.2/questions/"+String(questionId)+"/answers?&site=stackoverflow&filter=withbody") else {
+                return nil
+            }
+            self.url = url
+        }
+        
+        self.parse = parseJSON
+    }
+}
