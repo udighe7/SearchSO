40880fd0468df794403b0c71591e11472a6ee1d6
UtkarshDighe <utkarsh@helpshift.com>
2019-11-12 23:11:56 +0530

    This class will represent the Owner of StackOverflow questions and answers

---
+++ SearchSO/New Group/Owner.swift
@@ -0,0 +1,20 @@
+//
+//  Owner.swift
+//  SearchSO
+//
+//  Created by utkarsh on 12/11/19.
+//  Copyright © 2019 utkarsh. All rights reserved.
+//
+
+import Foundation
+
+class Owner {
+    let ownerId: Int
+    let ownerName: String
+    
+    init(id: Int, name: String) {
+        self.ownerId = id
+        self.ownerName = name
+    }
+}
+
