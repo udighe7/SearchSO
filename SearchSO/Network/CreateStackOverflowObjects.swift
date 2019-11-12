8a161f1eb6808d3a1a5941b1bab76863bbbd0629
UtkarshDighe <utkarsh@helpshift.com>
2019-11-12 23:50:49 +0530

    Added struct for Decoding the JSON response. Added function to CreateStackOverflowObject to return tuple of two objects for questions API call - SearchResult: To add a row of questionIds and search text in DB. Questions: To display questions to user.

--- SearchSO/Network/CreateStackOverflowObjects.swift
+++ SearchSO/Network/CreateStackOverflowObjects.swift
@@ -8,6 +8,37 @@
 
 import Foundation
 
+struct WrapperForQuestionAPI: Decodable {
+    let items: [Question]
+}
+
 struct CreateStackOverflowObjects {
     var jsonData: Data?
+    
+    func createObjectsForQuestionAPIResponse(query: String) -> (SearchResult?, [Question]?) {
+        guard let data = self.jsonData else {
+            return (nil,nil)
+        }
+        let wrapper = try? JSONDecoder().decode(WrapperForQuestionAPI.self, from: data)
+        
+        if let items = wrapper?.items {
+            
+            var listOfQuestionIds: [Int] = []
+            var listOfQuestions: [Question] = []
+            
+            for item in items {
+                listOfQuestionIds.append(item.questionId)
+                listOfQuestions.append(item)
+            }
+            
+            let questionResult = SearchResult(questionIds: listOfQuestionIds, query: query)
+            
+            return (questionResult, listOfQuestions)
+            
+        }
+        else {
+            return (nil,nil)
+        }
+    }
+
 }
