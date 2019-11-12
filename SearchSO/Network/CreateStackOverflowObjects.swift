dcdd339c8d5fbdb3760d96308e1cd602997a0481
UtkarshDighe <utkarsh@helpshift.com>
2019-11-12 23:46:35 +0530

    This struct would decode the JSON data obtained from the Network struct.

---
+++ SearchSO/Network/CreateStackOverflowObjects.swift
@@ -0,0 +1,13 @@
+//
+//  CreateStackOverflowObjects.swift
+//  SearchSO
+//
+//  Created by utkarsh on 12/11/19.
+//  Copyright © 2019 utkarsh. All rights reserved.
+//
+
+import Foundation
+
+struct CreateStackOverflowObjects {
+    var jsonData: Data?
+}
