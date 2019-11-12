1f1e7ea389b0440141960eb49ab1fa774ff126fc
UtkarshDighe <utkarsh@helpshift.com>
2019-11-13 01:21:19 +0530

    Added prototype cell in storyboard. Custom cell in storyboard has following fields - 1. Score - Tells question's score on StackOverflow 2. Votes - To display 'Votes' text 3. Image View - Green tick image will be shown in cell if the question has accepted answer 4. question - Consists of question title of StackOverflow question Added required constraints in storyboard.
    Created Cocoa Touch class called CustomTableViewCell to add necessary outlets to prototype cell.

---
+++ SearchSO/CustomTableViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  CustomTableViewCell.swift
+//  SearchSO
+//
+//  Created by utkarsh on 13/11/19.
+//  Copyright © 2019 utkarsh. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var questionScore: UILabel!
+    @IBOutlet weak var votesText: UILabel!
+    @IBOutlet weak var greenTickImage: UIStackView!
+    @IBOutlet weak var questionTitle: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
